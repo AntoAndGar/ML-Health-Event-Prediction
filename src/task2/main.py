346f668b76cacb10fa7e59dabf2b661a756d8b8f
Michele Spina <michelespina96@gmail.com>
2023-07-30 16:50:39 +0200

    initialize task2 importing anagrafica data and showing patient label values counts

---
+++ src/task2/main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+import random
+
+read_data_path  = "clean_data"
+
+df_anagrafica   = pd.read_csv(read_data_path+'/anagraficapazientiattivi_c.csv', header=0 ,names=['idana','idcentro','data','label'])
+#df_diagnosi = pd.read_csv(read_data_path+'/diagnosi_c.csv', header=0 ,names=['idcentro','idana','data','codiceamd','valore'])
+#df_esami_lab_par  = pd.read_csv(read_data_path+'/esamilaboratorioparametri_c.csv', header=0 ,names=['idcentro','idana','data','codiceamd','valore'])
+#df_esami_lab_par_calc  = pd.read_csv(read_data_path+'/esamilaboratorioparametricalcolati_c.csv', header=0 ,names=['idcentro','idana','data','codiceamd','valore'])
+#df_esami_stru   = pd.read_csv(read_data_path+'/esamistrumentali_c.csv', header=0 ,names=['idcentro','idana','data','codiceamd','valore'])
+#df_pre_diab_farm  = pd.read_csv(read_data_path+'/prescrizionidiabetefarmaci_c.csv', header=0 ,names=['idcentro','idana','data','codiceatc','quantita','idpasto','descrizionefarmaco'])
+#df_pre_diab_no_farm = pd.read_csv(read_data_path+'/prescrizionidiabetenonfarmaci_c.csv', header=0 ,names=['idcentro','idana','data','codiceamd','valore'])
+#df_pre_no_diab  = pd.read_csv(read_data_path+'/prescrizioninondiabete_c.csv', header=0 ,names=['idcentro','idana','data','codiceamd','valore'])
+
+### Point 2.1 ####
+df_anagrafica_w0 = df_anagrafica[df_anagrafica.label == 0]
+print(f"ap_class0 has size => {df_anagrafica_w0.shape}")
+df_anagrafica_w1 = df_anagrafica[df_anagrafica.label == 1]
+print(f"ap_class1 has size => {df_anagrafica_w1.shape}")
