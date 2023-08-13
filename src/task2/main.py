49eb8b716682826cc1c35e9095e8ee8f9e8b1dc8
antoandgar <antonioandgargiulo@gmail.com>
2023-08-13 17:37:43 +0200

    BERT datamodule working, not working BERTTransformer because of data

--- src/task2/main.py
+++ src/task2/main.py
@@ -4,6 +4,7 @@ import concurrent.futures as futures
 import multiprocessing
 import pickle
 
+from datetime import datetime
 from typing import Optional
 
 import torch
@@ -24,7 +25,7 @@ from transformers import (
     AutoModelForMaskedLM,
     get_linear_schedule_with_warmup,
 )
-from datasets import Dataset
+import datasets
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -782,7 +783,7 @@ def convert_to_huggingfaceDataset(tuple_dataset):
     # each containing the patient history string and their label
     # we need to convert it to a hugginface dataset
     dict_list = [{"label": data[1], "text": data[0]} for data in tuple_dataset]
-    dataset = Dataset.from_list(dict_list)
+    dataset = datasets.Dataset.from_list(dict_list)
     return dataset
 
 
@@ -805,8 +806,8 @@ class PubMedBERTDataModule(LightningDataModule):
             self.model_name_with_path, use_fast=True
         )
 
-    def setup(self):
-        dataset = convert_to_huggingfaceDataset(tuple_dataset)
+    def setup(self, stage=None):
+        dataset = convert_to_huggingfaceDataset(tuple_dataset[:50])
         tokenized_dataset = dataset.map(
             self.convert_to_features,
             batched=True,
@@ -859,10 +860,124 @@ class PubMedBERTDataModule(LightningDataModule):
         return features
 
 
+class PubMedBERTTransformer(LightningModule):
+    def __init__(
+        self,
+        model_name_or_path: str,
+        num_labels: int = 2,
+        learning_rate: float = 2e-5,
+        adam_epsilon: float = 1e-8,
+        warmup_steps: int = 0,
+        weight_decay: float = 0.0,
+        train_batch_size: int = 32,
+        eval_batch_size: int = 32,
+        eval_splits: Optional[list] = None,
+        **kwargs,
+    ):
+        super().__init__()
+
+        self.save_hyperparameters()
+
+        self.config = AutoConfig.from_pretrained(
+            model_name_or_path, num_labels=num_labels
+        )
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            model_name_or_path, config=self.config
+        )
+        self.metric = evaluate.load(
+            "accuracy", experiment_id=datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+        )
+
+    def forward(self, **inputs):
+        del inputs["label"]
+        del inputs["text"]
+        return self.model(
+            input_ids=inputs["input_ids"],
+            attention_mask=inputs["attention_mask"],
+            labels=inputs["labels"],
+        )
+
+    def training_step(self, batch, batch_idx):
+        outputs = self(**batch)
+        loss = outputs[0]
+        return loss
+
+    def validation_step(self, batch, batch_idx, dataloader_idx=0):
+        outputs = self(**batch)
+        val_loss, logits = outputs[:2]
+
+        if self.hparams.num_labels > 1:
+            preds = torch.argmax(logits, axis=1)
+        elif self.hparams.num_labels == 1:
+            preds = logits.squeeze()
+
+        labels = batch["labels"]
+
+        return {"loss": val_loss, "preds": preds, "labels": labels}
+
+    def on_validation_epoch_end(self, outputs):
+        preds = torch.cat([x["preds"] for x in outputs]).detach().cpu().numpy()
+        labels = torch.cat([x["labels"] for x in outputs]).detach().cpu().numpy()
+        loss = torch.stack([x["loss"] for x in outputs]).mean()
+        self.log("val_loss", loss, prog_bar=True)
+        self.log_dict(
+            self.metric.compute(predictions=preds, references=labels), prog_bar=True
+        )
+
+    def configure_optimizers(self):
+        """Prepare optimizer and schedule (linear warmup and decay)"""
+        model = self.model
+        no_decay = ["bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                "params": [
+                    p
+                    for n, p in model.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                ],
+                "weight_decay": self.hparams.weight_decay,
+            },
+            {
+                "params": [
+                    p
+                    for n, p in model.named_parameters()
+                    if any(nd in n for nd in no_decay)
+                ],
+                "weight_decay": 0.0,
+            },
+        ]
+        optimizer = AdamW(
+            optimizer_grouped_parameters,
+            lr=self.hparams.learning_rate,
+            eps=self.hparams.adam_epsilon,
+        )
+
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=self.hparams.warmup_steps,
+            num_training_steps=self.trainer.estimated_stepping_batches,
+        )
+        scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
+        return [optimizer], [scheduler]
+
+
 def evaluate_PubMedBERT():
-    # model = AutoModelForMaskedLM.from_pretrained(
-    #     "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
+    seed_everything(42)
+    model_name = "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
+
+    # dm = PubMedBERTDataModule(tuple_dataset, model_name)
+    # dm.setup("fit")
+    # model = PubMedBERTTransformer(
+    #     model_name_or_path=model_name,
     # )
+
+    # trainer = Trainer(
+    #     max_epochs=1,
+    #     accelerator="auto",
+    #     devices=1 if torch.cuda.is_available() else 1,
+    # )
+    # trainer.fit(model, datamodule=dm)
+
     return
 
 
